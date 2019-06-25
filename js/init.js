e2df6425570fb6fe1f627bdce10eefcf275aed4c
Armando Aguiar Alamilla <31995672+armandoalamilla@users.noreply.github.com>
2019-06-25 14:47:01 -0500

    fixed rs botones

--- js/init.js
+++ js/init.js
@@ -6,3 +6,18 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.getElementById("rs_insta").addEventListener("click", function abre() {
+
+  window.open("https://www.instagram.com/crosswaymty/");
+  
+});
+
+document.getElementById("rs_face").addEventListener("click", function abre() {
+
+  window.open("https://facebook.com/crosswaymty");
+  
+});
+
+
+
