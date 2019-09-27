e6174c32d9dbce546c4c9922e37eb00712188d91
Armando Aguiar Alamilla <31995672+armandoalamilla@users.noreply.github.com>
2019-09-26 23:10:43 -0500

    some fixes and sponsors added

--- js/init.js
+++ js/init.js
@@ -7,6 +7,8 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+//redes sociales
+
 document.getElementById("rs_insta").addEventListener("click", function abre() {
 
   window.open("https://www.instagram.com/crosswaymty/");
@@ -18,4 +20,44 @@ document.getElementById("rs_face").addEventListener("click", function abre() {
   window.open("https://facebook.com/crosswaymty");
   
 });
+
+//patrocinadores
+
+document.getElementById("cobrex").addEventListener("click", function abre() {
+
+  window.open("http://kobrex.mx/");
+  
+});
+
+document.getElementById("encino").addEventListener("click", function abre() {
+
+  window.open("https://www.facebook.com/elencinodesarrollosurbanos/");
+  
+});
+
+document.getElementById("galvasid").addEventListener("click", function abre() {
+
+  window.open("http://galvasid.com/");
+  
+});
+
+document.getElementById("indalum").addEventListener("click", function abre() {
+
+  window.open("https://indalum.com.mx/");
+  
+});
+
+document.getElementById("interceramic").addEventListener("click", function abre() {
+
+  window.open("https://interceramic.com/mx/");
+  
+});
+
+document.getElementById("perfiles").addEventListener("click", function abre() {
+
+  window.open("https://perfileslm.com/");
+  
+});
+
+
 
