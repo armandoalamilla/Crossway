be154999aab74f9b01c63b3616aff459925b4bd4
Armando Aguiar Alamilla <31995672+armandoalamilla@users.noreply.github.com>
2019-11-04 13:59:00 -0600

    some info done

--- js/conf.js
+++ js/conf.js
@@ -1,7 +1,147 @@
-$(document).ready(function(){
-    $('.collapsible').collapsible();
+//const el = document.createElement('div')
+//el.innerHTML = "Here's a <a href='http://google.com'>link</a>"
+
+var conf_1 = `LeNoir & Asociados
+Tema: Herramientas de la arquitectura
+Fecha y hora:  Miércoles 13 a las 10:00 a.m.
+Lugar: Centro de Congresos
+`
+
+$("#conf_1").click(function() {
+  swal({
+    title: "Alexandre Noir",
+    text: conf_1,
   });
-       
-  $(document).ready(function(){
-    $('.modal').modal();
+});
+
+var conf_2 = `Populous
+Tema: Arquitectura efímera en obras deportivas 
+Fecha y hora:  Jueves 14 a las 5:45 p.m.
+Lugar: Centro de Congresos
+`
+
+$("#conf_2").click(function() {
+  swal({
+    title: "Marc Klein, Arturo Fuentes",
+    text: conf_2,
   });
+});
+
+var conf_3 = `IDOM
+Tema: Proyecto del Estadio de Fútbol San Mamés Proceso de proyecto, obra e intervención estructural
+Fecha y hora: Miércoles 13 a las 11:30 a.m. 
+Lugar: Centro de Congresos
+`
+
+$("#conf_3").click(function() {
+  swal({
+    title: "César Aitor Azcárate Gómez",
+    text: conf_3,
+  });
+});
+
+var conf_4 = `MACH
+Tema: Fábrica de edificios
+Fecha y hora: Viernes 15 a las 9:00 a.m.
+Lugar: Centro de Congresos
+`
+
+$("#conf_4").click(function() {
+  swal({
+    title: "Miguel Chapa",
+    text: conf_4,
+  });
+});
+
+var conf_5 = `GLR Arquitectos
+Tema: Conversaciones con el paisaje
+Fecha y hora: Miércoles 13 a las 1:15 p.m. 
+Lugar: Centro de Congresos
+`
+$("#conf_5").click(function() {
+  swal({
+    title: "Gilberto L. Rodriguez",
+    text: conf_5,
+  });
+});
+
+var conf_6 = `Perkins and Will
+Tema: Temas relacionado con experiencia y procesos de trabajo en Perkins & Will / Resiliencia & Sustentabilidad / Trends en Educación
+Fecha y hora:  Jueves 14 a las 9:00 a.m. 
+Lugar: Centro de Congresos
+`
+$("#conf_6").click(function() {
+  swal({
+    title: "Antonio Perez",
+    text: conf_6,
+  });
+});
+
+var conf_7 = `Consultor independiente de Presas y Proyectos Hidroeléctricos 7
+Tema: Deslizamiento de tierra y roca del 2007 en el río grijalva, un caso de resiliencia resuelto exitosamente por la ingeniería mexicana.
+Fecha y hora:  Jueves 14 a las 9:30 am
+Lugar: Auditorio Biblioteca
+`
+$("#conf_7").click(function() {
+  swal({
+    title: "Humberto Marengo",
+    text: conf_7,
+  });
+});
+
+var conf_8 = `RECSA
+Tema: Retos para construir un méxico nuevo, túnel emisor poniente ii
+Fecha y hora: Jueves 14 a las 14 2:30
+Lugar: Centro de Congresos
+`
+$("#conf_8").click(function() {
+  swal({
+    title: "Andrés Alanís",
+    text: conf_8,
+  });
+});
+
+var conf_9 = `BEA
+Tema: Edificación Sustentable: Vanguardia y Tendencias Globales
+Fecha y hora:  Miércoles 13 a las 1:15 p.m.
+Lugar: Auditorio A6
+`
+$("#conf_9").click(function() {
+  swal({
+    title: "César Ulises Treviño",
+    text: conf_9,
+  });
+});
+
+var conf_10 = `Blue Green
+Tema: Historia de Sargablock
+Fecha y hora: Jueves 14 a las 4:00 p.m.
+Lugar: La Novela
+`
+$("#conf_10").click(function() {
+  swal({
+    title: "Omar Vazquez Sanchez",
+    text: conf_10,
+  });
+});
+
+var conf_11 = `Macia Estudios
+Tema: Territorios para el juego
+Fecha y hora: Viernes 15 a las 10:00 a.m.
+Lugar: Auditorio Bibliotec
+`
+$("#conf_11").click(function() {
+  swal({
+    title: "Mariana Rios Robles",
+    text: conf_11,
+  });
+});
+
+
+
+
+
+
+
+
+
